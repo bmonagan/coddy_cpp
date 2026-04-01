ddec41ad61ef9b594483c3f4f47285405443953b
bmonagan <bernardmonagan@gmail.com>
2026-04-01 00:15:58 -0400

    add initial implementation of int_chal.cpp

---
+++ int_chal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // Declare and initialize your variables here
+    int score = 100;
+    int bonus = 25;
+    
+    // Don't change below this line
+    std::cout << "Score: " << score << std::endl;
+    std::cout << "Bonus: " << bonus << std::endl;
+    
+    return 0;
+}
