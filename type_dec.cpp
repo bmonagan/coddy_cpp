d92e1e639459458d0ecd7584fca953f30b4b4676
bmonagan <bernardmonagan@gmail.com>
2026-04-02 23:27:20 -0400

    type dec

---
+++ type_dec.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Declaring variables
+    int count = 10;
+    double total = 150.75;
+    char grade = 'A';
+    bool isActive = false;
+    string userName = "Bob123";
+
+
+    // Printing values
+    cout << "Count: " << count << endl;
+    cout << "Total: " << total << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Active: " << isActive << endl;
+    cout << "User Name: " << userName << endl;
+
+    return 0;
+}
