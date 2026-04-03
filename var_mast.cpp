e3682006e0b86156f396702ad011b90d36287278
bmonagan <bernardmonagan@gmail.com>
2026-04-02 23:21:09 -0400

    var mastery

---
+++ var_mast.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    // Declare your variables here
+    // Tip: float uses the 'f' suffix (e.g. 4.5f), but double uses NO suffix (e.g. 3.1415)
+    float radius = 4.5f;
+    double pi = 3.1415;
+    
+    // Don't change below this line
+    std::cout << "radius: " << radius << std::endl;
+    std::cout << "pi: " << pi << std::endl;
+    
+    return 0;
+}
