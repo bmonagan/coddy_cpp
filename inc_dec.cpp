12d8d083e41541d5b4d747b50b856e5c9ce7a9f5
bmonagan <bernardmonagan@gmail.com>
2026-04-03 22:51:30 -0400

    inc dec

---
+++ inc_dec.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int count = 0;
+    
+    // Type your code below
+    count++;
+    count++;
+    count++;
+    count++;
+    count *=2;
+    count--;
+    
+    
+    // Don\'t change the line below
+    std::cout << "count = " << count;
+    return 0;
+}
