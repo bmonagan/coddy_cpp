c13116d88845faa4cc62db591feedee8e6289086
bmonagan <bernardmonagan@gmail.com>
2026-04-03 22:47:10 -0400

    operators

---
+++ operators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    // Type your code below
+    double a = 5.2;
+    double b = 2.6;
+    double c = a/b;
+    
+    // Don't change the line below
+    std::cout << "a = " << a << ", b = " << b << ", c = " << c;
+    return 0;
+}
