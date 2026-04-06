c488aaab3917802b81dab0f0a36601bc2957ee5f
bmonagan <bernardmonagan@gmail.com>
2026-04-05 23:01:23 -0400

    reset

---
+++ artith_shortcuts.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int count = 0;
+    
+    // Type your code below
+    count += 4;
+    count *= 2;
+    count -= 1;
+    
+    // Don\'t change the line below
+    std::cout << "count = " << count;
+    return 0;
+}
---
+++ comparison.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // Type your code below
+
+    int n1 = 8;
+    int n2 = 9;
+    int n3 = n1 > n2;
+    // Don't change the line below
+    std::cout << "n1 = " << n1 << ", n2 = " << n2 << ", n3 = " << n3 << std::endl;
+    
+    return 0;
+}
