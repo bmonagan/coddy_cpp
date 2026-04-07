dc3308f916eec294accbe8e81c25c5175f8f0ad5
bmonagan <bernardmonagan@gmail.com>
2026-04-06 23:47:28 -0400

    add initial implementation of logical operations example

---
+++ logical_op1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    // Type your code below
+    bool b1 = true;
+    bool b2 = false;
+    bool b3 = b1 || b2;
+    
+    // Don't change the line below
+    std::cout << "b3 = " << b3;
+    return 0;
+}
