12fd4f858d98918bc8ec6f264c4184e679e092da
bmonagan <bernardmonagan@gmail.com>
2026-04-06 23:48:31 -0400

    add implementation of logical operations example

---
+++ logical_op2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Type your code below
+    int b1 = 6;
+    int b2 = 7;
+    bool b3 = !((b1 + b2) > (b1 * b2));
+    
+    // Don't change the line below
+    cout << "b3 = " << b3 << endl;
+    
+    return 0;
+}
