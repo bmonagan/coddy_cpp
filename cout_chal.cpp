f7ad5aa77b31907ff0266d4c887a9f1511354146
bmonagan <bernardmonagan@gmail.com>
2026-04-08 23:41:02 -0400

    add initial implementation of cout example demonstrating basic C++ features

---
+++ cout_chal.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "C++ is fun!\n";
+    std::cout << "Let's explore its features:\n";
+    std::cout<< "1. Variables"<< "\t" << "2. Loops\n";
+    std::cout << "3. Functions" << "\t" << "4. Classes\n";
+    std::cout << "Happy coding!";
+    return 0;
+};
