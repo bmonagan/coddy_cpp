b01124713ad5426a5ed734edcee714e28609a505
bmonagan <bernardmonagan@gmail.com>
2026-04-09 21:42:19 -0400

    add initial implementation of calculator app to get user input

---
+++ challenge_getting_numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Calculator App" << endl;
+    float first_number;
+    float second_number;
+    cin >> first_number;
+    cin >> second_number;
+    cout << "First number: " << first_number << endl;
+    cout << "Second number: " << second_number << endl;
+
+    return 0;
+}
