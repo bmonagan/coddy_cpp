d59a5d4d7996c753e44877098b08cdd0437d1e86
bmonagan <bernardmonagan@gmail.com>
2026-04-10 23:42:31 -0400

    add for_loop implementation to print greeting messages

---
+++ for_loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // Write your code below
+    
+    for (int i = 3; i <= 27 ; i++){
+        std::cout << "Hello Coddy: " << i << "\n";
+    }
+    
+    return 0;
+}
