654353fe6eb2fe7f6190058e5d26f824780e2c15
bmonagan <bernardmonagan@gmail.com>
2026-04-11 23:47:05 -0400

    add implementation of for loop with continue statement to print even numbers from 1 to 20

---
+++ continue.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 20; i++) {
+        if (i % 2 == 1){
+            continue;
+        }
+        std::cout << i << std::endl;
+    }
+    return 0;
+}
