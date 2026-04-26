4a437a7aea6ef403c3ac9750aab7a1a1c7c30692
bmonagan <bernardmonagan@gmail.com>
2026-04-25 22:32:35 -0400

    task_list tool part 1.

---
+++ task_list_tool.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    vector<string> tasks;
+    cout << "Welcome to Task List Tool!" << endl;
+    cout << endl;
+    cout << "Menu Options:" << endl;
+    cout << "1. Add Task" << endl;
+    cout << "2. View Tasks" << endl;
+    cout << "3. Quit" << endl;
+    cout << endl;
+    cout << "Task list system initialized and ready!" << endl;
+    
+    return 0;
+}
